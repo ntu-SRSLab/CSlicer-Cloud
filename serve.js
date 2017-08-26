04931ade9f98ca18683d21ba012ce4cb993208e5
Yi Li <liyi0630@gmail.com>
2017-08-26 02:16:35 +0000

    serve.js edited online with Bitbucket

--- serve.js
+++ serve.js
@@ -1,18 +1,13 @@
 /*
-This file is part of git-flow-json-commits.
+This file is part of cslicer-cloud.
 
-git-flow-json-commits is free software: you can redistribute it and/or modify
+cslicer-cloud is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-GitFlowVisualize is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
-along with git-flow-json-commits. If not, see <http://www.gnu.org/licenses/>.
+along with cslicer-cloud. If not, see <http://www.gnu.org/licenses/>.
 */
 
 
